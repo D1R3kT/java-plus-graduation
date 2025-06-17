9187ce7fc15e1f85cc2b9dce5e8d1181a9c41949
David_Yegorov <egor0vdavid@yandex.ru>
2025-06-17 18:52:20 +0300

    microservices-V1

--- core/comment-service/src/main/java/ru/practicum/comment/service/repository/BanCommentRepository.java
+++ core/comment-service/src/main/java/ru/practicum/comment/service/repository/BanCommentRepository.java
@@ -1,9 +1,12 @@
 package ru.practicum.comment.service.repository;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import ru.practicum.comment.service.model.BanComment;
+import ru.practicum.comment.service.model.Comment;
 
-public interface BanCommentRepository extends JpaRepository<BanComment, Long> {
+import java.util.List;
 
-    BanComment findByUserIdAndEventId(Long userId, Long eventId);
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findAllByEventId(Long id, Pageable pageable);
 }
